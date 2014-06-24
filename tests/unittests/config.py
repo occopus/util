4032be18b47112ef363b2b5d4441fe7cb5d161e4
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-06-24 16:30:58 +0200

    OCD-23 New unit test for occo.util.config

--- tests/unittests/config.py
+++ tests/unittests/config.py
@@ -22,12 +22,19 @@ class ConfigTest(unittest.TestCase):
     def test_load(self):
         self.args.parse_args()
         self.assertDictContainsSubset(self.ethalon, self.args.__dict__)
+    def test_override(self):
+        testargs = '--setting_2=ttt -s1 55'.split()
+        self.args.add_argument('--setting_1', '-s1', type=int)
+        self.args.add_argument('--setting_2', '-s2')
+        self.args.parse_args(testargs)
+        self.assertEqual(self.args.setting_1, 55)
+        self.assertEqual(self.args.setting_2, 'ttt')
 
 class ConfigTestSuite(unittest.TestSuite):
     def __init__(self):
         unittest.TestSuite.__init__(
             self, map(ConfigTest, ['test_load',
-                                   ]))
+                                   'test_override',]))
 
 if __name__ == '__main__':
     alltests = unittest.TestSuite([ConfigTestSuite(),
