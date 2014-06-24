96410e035a15356571a0ad63c72071082f5421c2
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-06-24 12:53:50 +0200

    OCD-23 Implement unit test for occo.util.config

---
+++ tests/unittests/config.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2014 MTA SZTAKI
+#
+# Configuration primitives for the SZTAKI Cloud Orchestrator
+#
+
+import unittest
+from common import *
+import occo.util.config as cfg
+
+class ConfigTest(unittest.TestCase):
+    def setUp(self):
+        self.filename = 'test_config.yaml'
+        self.ethalon = dict(
+            setting_1=1,
+            setting_2=2,
+            complex_setting=dict(cs_1=3, cs_2=4, cs_3=5),
+            list_setting=['a', 'b', 'c']
+        )
+    def test_load(self):
+        args = cfg.DefaultYAMLConfig(self.filename)
+        args.parse_args()
+        self.assertEqual(self.ethalon, args.__dict__)
+
+class ConfigTestSuite(unittest.TestSuite):
+    def __init__(self):
+        unittest.TestSuite.__init__(
+            self, map(ConfigTest, ['test_load',
+                                   ]))
+
+if __name__ == '__main__':
+    alltests = unittest.TestSuite([ConfigTestSuite(),
+                                   ])
+    unittest.main()
