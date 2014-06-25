00a11b09055ed7d06cdc65c81908fc258e880af1
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-06-25 11:30:15 +0200

    Create skeleton for communication.mq
    OCD-21

--- occo/util/communication/mq.py
+++ occo/util/communication/mq.py
@@ -4,6 +4,23 @@
 # Configuration primitives for the SZTAKI Cloud Orchestrator
 #
 
-__all__ = []
+__all__ = ['MQHandler', 'MQAsynchronProducer', 'MQRPCProducer',
+           'MQEventDriverConsumer']
 
 import occo.util.communication as com
+
+class MQHandler(object):
+    def __init__(self, **config):
+        pass
+
+class MQAsynchronProducer(MQHandler, com.AsynchronProducer):
+    def __init__(self, **config):
+        pass
+
+class MQRPCProducer(MQHandler, com.RPCProducer):
+    def __init__(self, **config):
+        pass
+
+class MQEventDrivenConsumer(MQHandler, com.EventDrivenConsumer):
+    def __init__(self, **config):
+        pass
