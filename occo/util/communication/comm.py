6a416eda087ed47f74cfce19d044eaf23913ae2c
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-06-25 11:47:15 +0200

    Enable comm classes to select backend automatically
    Factory design pattern based on python __new__
    
    OCD-21 #time 30m

--- occo/util/communication/comm.py
+++ occo/util/communication/comm.py
@@ -4,21 +4,44 @@
 # Configuration primitives for the SZTAKI Cloud Orchestrator
 #
 
-__all__ = ['AsynchronProducer', 'RPCProducer', 'EventDrivenConsumer']
+__all__ = ['AsynchronProducer', 'RPCProducer', 'EventDrivenConsumer', 'register']
 
-class AsynchronProducer(object):
+class ConfigurationError(Exception):
+    pass
+
+class RegisteredBackend(object):
+    def __init__(self, target, id_):
+        self.target = target
+        self.id_ = id_
+    def __call__(self, cls):
+        if not hasattr(self.target, 'backends'):
+            self.target.backends = dict()
+        self.target.backends[self.id_] = cls
+        return cls
+register = RegisteredBackend
+
+class MultiBackend(object):
+    def __new__(cls, *args, **kwargs):
+        try:
+            protocol = kwargs['protocol']
+        except KeyError:
+            raise ConfigurationError('Missing protocol specification')
+        else:
+            return cls.backends[protocol](*args, **kwargs)
+
+class AsynchronProducer(MultiBackend):
     def __init__(self):
         pass
     def push_message(self, message, **kwargs):
         raise NotImplementedError
 
-class RPCProducer(object):
+class RPCProducer(MultiBackend):
     def __init__(self):
         pass
     def push_message(self, message, **kwargs):
         raise NotImplementedError
 
-class EventDriveConsumer(object):
+class EventDriveConsumer(MultiBackend):
     def __init__(self):
         pass
     def start_consuming(self, processor, **kwargs):
--- occo/util/communication/mq.py
+++ occo/util/communication/mq.py
@@ -9,18 +9,23 @@ __all__ = ['MQHandler', 'MQAsynchronProducer', 'MQRPCProducer',
 
 import occo.util.communication as com
 
+PROTOCOL_ID='amqp'
+
 class MQHandler(object):
     def __init__(self, **config):
         pass
 
+@com.register(com.AsynchronProducer, PROTOCOL_ID)
 class MQAsynchronProducer(MQHandler, com.AsynchronProducer):
     def __init__(self, **config):
         pass
 
+@com.register(com.RPCProducer, PROTOCOL_ID)
 class MQRPCProducer(MQHandler, com.RPCProducer):
     def __init__(self, **config):
         pass
 
+@com.register(com.EventDrivenConsumer, PROTOCOL_ID)
 class MQEventDrivenConsumer(MQHandler, com.EventDrivenConsumer):
     def __init__(self, **config):
         pass
