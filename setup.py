169fbafbd230c84819c00618fb0e94193a73c12e
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-06-25 12:53:29 +0200

    Fixup occo.util package with communication
    OCD-22

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     version='0.1.0',
     author='Adam Visegradi',
     author_email='adam.visegradi@sztaki.mta.hu',
-    packages=['occo.util', 'occo.util.config'],
+    packages=['occo.util', 'occo.util.config', 'occo.util.communication'],
 #    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
     url='http://www.lpds.sztaki.hu/',
     license='LICENSE.txt',
