d5e8b8b95e8c52630e4aa9329bfcd7e4257d68bb
Novak Adam <novak.adam@sztaki.mta.hu>
2014-07-02 11:37:29 +0000

    implement RPC message Queue
    OCD-21 #time 2h

--- occo/util/communication/mq.py
+++ occo/util/communication/mq.py
@@ -18,12 +18,43 @@ class MQHandler(object):
 @comm.register(comm.AsynchronProducer, PROTOCOL_ID)
 class MQAsynchronProducer(MQHandler, comm.AsynchronProducer):
     def __init__(self, **config):
-        pass
+        self.host = config['host']
 
 @comm.register(comm.RPCProducer, PROTOCOL_ID)
 class MQRPCProducer(MQHandler, comm.RPCProducer):
     def __init__(self, **config):
-        pass
+
+        self.credentials = pika.PlainCredentials(config['user'],config['password'])
+        self.connectionParameters = pika.ConnectionParameters(config['host'],config['port'],
+config['virtual_host'],self.credentials)
+        self.connection = pika.BlockingConnection(connectionParameters)
+        self.channel =self.connection.channel()
+
+	returnValue = self.channel.queue_declare(exclusive=True)
+	self.callback_queue = returnValue.method.queue
+
+	self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
+
+
+    def on_response(self, ch, method, props, body):
+	if self.corr_id == props.correlation_id:
+	    self.response = body
+
+
+    def push_msg(self, msg, **kwargs):
+
+	self.response = None
+	self.corr_id =uuid.uuid4())
+
+#TODO: routing key is permanent, solution needed (routing key will be part of msg)
+	self.channel.basic_publish(exchange='', routing_key='rpc_queue',
+properties=pika.BasicProperties(reply_to = self.callback_queue, correlation_id = self.corr_id), body = msg)
+	
+	while self.response is None:
+	    self.connection.process_data_events()
+	return self.response
+
+	pass
 
 @comm.register(comm.EventDrivenConsumer, PROTOCOL_ID)
 class MQEventDrivenConsumer(MQHandler, comm.EventDrivenConsumer):
