96426a27c4da0c00a86ae4a9fb205e2b9f02ea06
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-07-15 15:23:35 +0200

    Fix push_message signature
    OCD-21

--- occo/util/communication/mq.py
+++ occo/util/communication/mq.py
@@ -73,7 +73,7 @@ class MQRPCProducer(MQHandler, comm.RPCProducer):
         if self.correlation_id == props.correlation_id:
             self.response = body
 
-    def push_message(self, msg, routing_key, **kwargs):
+    def push_message(self, msg, routing_key=None, **kwargs):
         if self.correlation_id != None:
             raise RuntimeError('pika is not thread safe.')
 
