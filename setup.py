50d5744d4a986e72695b084d284c576f33a53ded
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-16 16:51:16 +0100

    Fixup setup script because pip update

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import setuptools
 from pip.req import parse_requirements
 
-reqs = [ str(i.req) for i in parse_requirements('requirements.txt') ]
-
 setuptools.setup(
     name='OCCO-Util',
     version='0.1.0',
@@ -17,5 +15,12 @@ setuptools.setup(
     license='LICENSE.txt',
     description='OCCO Utility Modules',
     long_description=open('README.txt').read(),
-    install_requires=reqs,
+    install_requires=['PyYAML',
+                      'argparse',
+                      'dateutils',
+                      'pika',
+                      'python-dateutil',
+                      'pytz',
+                      'six',
+                      'wsgiref' ],
 )
