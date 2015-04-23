e9c07f4742a219c74275054aed6f1d92f989e4b3
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-04-23 10:54:05 +0000

    Update file paths after changing relative path resolution

--- occo_test/mq_test.py
+++ occo_test/mq_test.py
@@ -17,7 +17,7 @@ import logging.config
 import uuid
 import time
 
-util.set_config_base_dir('etc/occo')
+util.set_config_base_dir(__file__, use_dir=True)
 CFG_FILE=util.cfg_file_path('comm_test_cfg.yaml')
 with open(CFG_FILE) as cfg:
     cfg = config.DefaultYAMLConfig(cfg)
