21ac9d90564024b092c5fdbfbb8240eb46c49478
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-08-14 12:38:01 +0000

    Generalize dry_run facilities

--- occo/util/__init__.py
+++ occo/util/__init__.py
@@ -377,9 +377,18 @@ class wet_method(object):
 
         @functools.wraps(fun)
         def wethod(fun_self_, *args, **kwargs):
-            if getattr(fun_self_, 'dry_run', False):
+            def sources():
+                yield 'object', fun_self_
+            def getvalue(o):
+                return getattr(o, 'dry_run', False)
+            dry_run_set_at = next(
+                (src for src, value in pair_map(sources(), getvalue) if value),
+                None)
+            if dry_run_set_at is not None:
                 log = logging.getLogger('occo.util')
-                log.warning('Dry run: omitting method execution for %s.%s.%s',
+                log.warning('Dry run (specified at %s level): '
+                            'omitting method execution for %s.%s.%s',
+                            dry_run_set_at,
                             fun_self_.__class__.__module__,
                             fun_self_.__class__.__name__,
                             fun.__name__)
