473e13ba1e45e85c3648ef1ecc6701288061b08b
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2017-04-20 14:38:37 +0000

    Change from bash to sh in installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Installing Occopus..."
 
@@ -8,25 +8,24 @@ if [ -d "$HOME/occopus" ] || [ -d "$HOME/.occopus" ]; then
   exit 1
 fi
 
-sudo apt-get -y -qq install curl
-sudo apt-get -y -qq install python python-pip python-dev python-virtualenv 
+sudo apt-get -y -qq install python python-pip python-dev python-virtualenv
 sudo apt-get -y -qq install libffi-dev libssl-dev
 sudo apt-get -y -qq install redis-server mysql-client
 
-virtualenv $HOME/occopus; 
-source $HOME/occopus/bin/activate; 
-pip install --upgrade pip; 
+virtualenv $HOME/occopus
+. $HOME/occopus/bin/activate
+pip install --upgrade pip
 pip install --find-links http://pip.lpds.sztaki.hu/packages \
-	    --no-index --trusted-host pip.lpds.sztaki.hu OCCO-API;
-mkdir -p $HOME/.occopus;
+	    --no-index --trusted-host pip.lpds.sztaki.hu OCCO-API
+mkdir -p $HOME/.occopus
 curl -s https://raw.githubusercontent.com/occopus/docs/devel/tutorial/.occopus/occopus_config.yaml \
-     -o $HOME/.occopus/occopus_config.yaml; 
+     -o $HOME/.occopus/occopus_config.yaml 
 curl -s https://raw.githubusercontent.com/occopus/docs/devel/tutorial/.occopus/auth_data.yaml \
-     -o $HOME/.occopus/auth_data.yaml;
-echo;
+     -o $HOME/.occopus/auth_data.yaml
+echo
 echo "Configuration file and authentication template have been copied to"\
-     "\"$HOME/.occopus\" directory.";
-echo "Fill authentication template with credentials before using Occopus!";
-echo "Do not forget to activate your virtualenv before using Occopus!";
-echo "Run \"source $HOME/occopus/bin/activate\"";
+     "\"$HOME/.occopus\" directory."
+echo "Fill authentication template with credentials before using Occopus!"
+echo "Do not forget to activate your virtualenv before using Occopus!"
+echo "Run \"source $HOME/occopus/bin/activate\""
 
